4d3d6419f8a543efcdc87a636e9352e018616988
chensiyu <chensiyu1029@qq.com>
2025-09-07 15:16:14 +0800

    初始化项目

---
+++ src/test/java/com/study/aiagent/AiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.aiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
